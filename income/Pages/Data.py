6c06378b4fe2029ca6a8c3f3a9690e84b4eb078a
Ziad Elgendy <165091274+ZiadElgendy21@users.noreply.github.com>
2024-09-06 06:14:29 +0300

    Update Data.py

--- income/Pages/Data.py
+++ income/Pages/Data.py
@@ -2,6 +2,6 @@ import pandas as pd
 import streamlit as st 
 
 st.markdown("<h1> <center> Income Prediction DataSet </center> </h1>",unsafe_allow_html= True)
-df = pd.read_csv("E:/Mid_Project_Data_Science/income/Sources/adult.csv")
+df = pd.read_csv("./income/Sources/adult.csv")
 st.markdown('<a href= "https://www.kaggle.com/datasets/wenruliu/adult-income-dataset/data"> <center> Link to DataSet </center> </a>',unsafe_allow_html= True)
 st.write(df)
