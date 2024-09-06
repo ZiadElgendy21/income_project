5d50754ae5dcc71690b2b7de07a334a49e5ce14d
Ziad Elgendy <165091274+ZiadElgendy21@users.noreply.github.com>
2024-09-06 06:13:42 +0300

    Update Home.py

--- income/Home.py
+++ income/Home.py
@@ -7,4 +7,4 @@ st.markdown("<h1> <center> Income prediction </center> </h1>",unsafe_allow_html=
 st.markdown(''' <center>  <h6>
     This app is created to analyze data about income prediction</center> </h6> ''', unsafe_allow_html=True)
 
-st.image("E:\Mid_Project_Data_Science\income\Sources\Income_Prediction_Streamlit.png")
+st.image("./income/Sources/Income_Prediction_Streamlit.png")
